a36d01fefc6866a3bc513064acb7197d64dd8101
saka <saka31.sw@gmail.com>
2024-09-12 10:50:00 +0700

    update code g.gks

--- js/gedung_gks.js
+++ js/gedung_gks.js
@@ -1,18 +1,44 @@
 export function initializeGedungGKS(map) {
-    // Base polygon for Gedung GKS
-    var gedung_gks = L.polygon([
+    // Define polygons and markers for Gedung GKS and its floors
+    const gedungGKSCoords = [
         [-7.052203, 110.434865],
         [-7.052209, 110.435174],
         [-7.052510, 110.435166],
         [-7.052499, 110.434868]
-    ], {
+    ];
+
+    const lantai3Coords = gedungGKSCoords;
+    const lantai2Coords = gedungGKSCoords;
+    const lantai1Coords = gedungGKSCoords;
+
+    
+    // Create the polygon layers
+    const gedungGKS = L.polygon(gedungGKSCoords, {
         color: 'rgb(255,255,255)',
         fillColor: 'rgb(162, 190, 227)',
         fillOpacity: 0.5
     }).addTo(map);
 
-    // Marker for Gedung GKS text
-    var teksGedung_gks = L.marker([-7.052358, 110.435002], {
+    const lantai3GKS = L.polygon(lantai3Coords, {
+        color: 'rgb(106, 156, 137)',
+        fillColor: 'rgb(255,255,255)',
+        fillOpacity: 0.7
+    });
+
+    const lantai2GKS = L.polygon(lantai2Coords, {
+        color: 'rgb(201, 104, 104)',
+        fillColor: 'rgb(255,255,255)',
+        fillOpacity: 0.7
+    });
+
+    const lantai1GKS = L.polygon(lantai1Coords, {
+        color: 'rgb(34, 123, 148)',
+        fillColor: 'rgb(255,255,255)',
+        fillOpacity: 0.7
+    });
+
+    // Create the text markers
+    const teksGedungGKS = L.marker([-7.052358, 110.435002], {
         icon: L.divIcon({
             className: 'text-label',
             html: 'Gedung <br>Kerjasama',
@@ -21,46 +47,7 @@ export function initializeGedungGKS(map) {
         interactive: false
     }).addTo(map);
 
-    // Warna denah perlantai
-    //lantai 3
-    var lantai3_gks = L.polygon([
-        [-7.052203, 110.434865],
-        [-7.052209, 110.435174],
-        [-7.052510, 110.435166],
-        [-7.052499, 110.434868]
-    ], {
-        color: 'rgb(106, 156, 137)',
-        fillColor: 'rgb(255,255,255)',
-        fillOpacity: 0.7
-    });
-
-    // lantai 2
-    var lantai2_gks = L.polygon([
-        [-7.052203, 110.434865],
-        [-7.052209, 110.435174],
-        [-7.052510, 110.435166],
-        [-7.052499, 110.434868]
-    ], {
-        color: 'rgb(201, 104, 104)',
-        fillColor: 'rgb(255,255,255)',
-        fillOpacity: 0.7
-    });
-
-    //lantai 1
-    var lantai1_gks = L.polygon([
-        [-7.052203, 110.434865],
-        [-7.052209, 110.435174],
-        [-7.052510, 110.435166],
-        [-7.052499, 110.434868]
-    ], {
-        color: 'rgb(34, 123, 148)',
-        fillColor: 'rgb(255,255,255)',
-        fillOpacity: 0.7
-    });
-
-    // teks perlantai
-    //lantai 3
-    var teksLantai3 = L.marker([-7.052358, 110.435002], {
+    const teksLantai3 = L.marker([-7.052358, 110.435002], {
         icon: L.divIcon({
             className: 'text-label-dua',
             html: 'Lantai 3',
@@ -69,8 +56,7 @@ export function initializeGedungGKS(map) {
         interactive: false
     });
 
-    //lantai 2
-    var teksLantai2 = L.marker([-7.052358, 110.435002], {
+    const teksLantai2 = L.marker([-7.052358, 110.435002], {
         icon: L.divIcon({
             className: 'text-label-dua',
             html: 'Lantai 2',
@@ -79,8 +65,7 @@ export function initializeGedungGKS(map) {
         interactive: false
     });
 
-    //lantai 1
-    var teksLantai1 = L.marker([-7.052358, 110.435002], {
+    const teksLantai1 = L.marker([-7.052358, 110.435002], {
         icon: L.divIcon({
             className: 'text-label-dua',
             html: 'Lantai 1',
@@ -91,13 +76,13 @@ export function initializeGedungGKS(map) {
 
     let currentLantai = 0;
 
-    // fungsi untuk menganti tampilan lantai
+    // Function to update floor visibility
     function updateFloorVisibility(lantai) {
-        map.removeLayer(gedung_gks);
-        map.removeLayer(teksGedung_gks);
-        map.removeLayer(lantai1_gks);
-        map.removeLayer(lantai2_gks);
-        map.removeLayer(lantai3_gks);
+        map.removeLayer(gedungGKS);
+        map.removeLayer(teksGedungGKS);
+        map.removeLayer(lantai1GKS);
+        map.removeLayer(lantai2GKS);
+        map.removeLayer(lantai3GKS);
         map.removeLayer(teksLantai1);
         map.removeLayer(teksLantai2);
         map.removeLayer(teksLantai3);
@@ -106,22 +91,22 @@ export function initializeGedungGKS(map) {
 
         switch (lantai) {
             case 0:
-                map.addLayer(gedung_gks);
-                map.addLayer(teksGedung_gks);
+                map.addLayer(gedungGKS);
+                map.addLayer(teksGedungGKS);
                 map.setView([-7.052358, 110.435002], 19);
                 break;
             case 1:
-                map.addLayer(lantai1_gks);
+                map.addLayer(lantai1GKS);
                 map.addLayer(teksLantai1);
                 map.setView([-7.052358, 110.435002], 22);
                 break;
             case 2:
-                map.addLayer(lantai2_gks);
+                map.addLayer(lantai2GKS);
                 map.addLayer(teksLantai2);
                 map.setView([-7.052358, 110.435002], 22);
                 break;
             case 3:
-                map.addLayer(lantai3_gks);
+                map.addLayer(lantai3GKS);
                 map.addLayer(teksLantai3);
                 map.setView([-7.052358, 110.435002], 22);
                 break;
@@ -129,7 +114,7 @@ export function initializeGedungGKS(map) {
     }
 
     // Click event to zoom into the building
-    gedung_gks.on('click', function () {
+    gedungGKS.on('click', function () {
         map.flyTo([-7.052355, 110.434898], 18, {
             duration: 2,
             easeLinearity: 0.5,
