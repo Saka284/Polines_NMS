08d027ff7a4c3ee58249fa9aba84f7dfe6b83463
saka <saka31.sw@gmail.com>
2024-09-13 16:12:48 +0700

    lantai 3 gks

--- js/gedung_gks.js
+++ js/gedung_gks.js
@@ -58,6 +58,25 @@ export function initializeGedungGKS(map) {
         { coords: [[-7.052278, 110.435090], [-7.052278, 110.435138], [-7.052310, 110.435138], [-7.052310, 110.435090]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Class', icon: 'images/classroom.png' },
     ];
 
+    const ruanganCoordsgksLt3 = [
+        //Classroom
+        { coords: [[-7.052205, 110.434867], [-7.052207, 110.434951], [-7.052304, 110.434950], [-7.052302, 110.434865]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Classroom', icon: 'images/classroom.png' },
+        //Studio 1
+        { coords: [[-7.052302, 110.434865], [-7.0523039, 110.434950], [-7.052411, 110.434949], [-7.05241, 110.434862]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Studio 1', icon: 'images/studio.png' },
+        //Studio 2
+        { coords: [[-7.05241, 110.434862], [-7.052411, 110.435008], [-7.052501, 110.435008], [-7.052500, 110.4348601]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Studio 2', icon: 'images/studio.png' },
+        //ruangan transmition
+        { coords: [[-7.052450, 110.435090], [-7.052450, 110.435160], [-7.052502, 110.435160], [-7.052502, 110.435090]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Transmition<br>room', icon: 'images/transmition.png' },
+        //Master Control Room
+        { coords: [[-7.052450, 110.435050], [-7.052450, 110.435090], [-7.052502, 110.435090], [-7.052502, 110.435050]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Master<br>Control<br>Room', icon: 'images/control.png' },
+        //Property Room
+        { coords: [[-7.052450, 110.435008], [-7.052450, 110.435050], [-7.052502, 110.435050], [-7.052502, 110.435008]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'Property<br>Room', icon: 'images/property.png' },
+        //R Dosen
+        { coords: [[-7.052420, 110.435105], [-7.052420, 110.435160], [-7.052450, 110.435160], [-7.052450, 110.435105]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: 'R. Dosen', icon: 'images/room.png' },
+        //lobby
+        { coords: [[-7.052304, 110.434950], [-7.0523039, 110.435008], [-7.052411, 110.435008], [-7.05241, 110.434949]], color: 'rgb(0, 255, 0)', fillColor: 'rgb(150, 255, 150)', text: '', icon: '' },
+    ];
+
     // Create the polygon layers
     const gedungGKS = L.polygon(gedungGKSCoords, {
         color: 'rgb(255,255,255)',
@@ -179,6 +198,36 @@ export function initializeGedungGKS(map) {
         return { polygon, textMarker };
     });
 
+    const ruanganLayersgksLt3 = ruanganCoordsgksLt3.map(room => {
+        const polygon = L.polygon(room.coords, {
+            color: room.color,
+            fillColor: room.fillColor,
+            fillOpacity: 0.7
+        });
+
+
+        // Create a marker for the room with optional icon and text
+        const textMarker = L.marker([
+            (room.coords[0][0] + room.coords[2][0]) / 2,
+            (room.coords[0][1] + room.coords[2][1]) / 2
+        ], {
+            icon: L.divIcon({
+                className: 'text-label',
+                html: `
+                    <div style="text-align: center; display: flex; flex-direction: column; justify-content: center; align-items: center; height: 60px;">
+                        ${room.icon ? `<img src="${room.icon}" style="width: 20px; height: 20px;" />` : ''}
+                        ${room.text ? `<div style="margin-top: 4px; font-size: 10px; color: black;">${room.text}</div>` : ''}
+                    </div>`,
+                iconSize: [80, 60], // Adjust iconSize to fit the icon and text
+                iconAnchor: [40, 30], // Adjust iconAnchor to center the icon properly
+                popupAnchor: [0, -60] // Adjust popupAnchor if you want to position the popup differently
+            }),
+            interactive: false
+        });
+
+        return { polygon, textMarker };
+    });
+
     let currentLantai = 0;
 
     // Function to update floor visibility
@@ -196,6 +245,10 @@ export function initializeGedungGKS(map) {
             map.removeLayer(layer.textMarker);
         });
         ruanganLayersgksLt2.forEach(layer => {
+            map.removeLayer(layer.polygon);
+            map.removeLayer(layer.textMarker);
+        });
+        ruanganLayersgksLt3.forEach(layer => {
             map.removeLayer(layer.polygon);
             map.removeLayer(layer.textMarker);
         });
@@ -229,6 +282,10 @@ export function initializeGedungGKS(map) {
             case 3:
                 map.addLayer(lantai3GKS);
                 map.addLayer(teksLantai3);
+                ruanganLayersgksLt3.forEach(layer => {
+                    map.addLayer(layer.polygon);
+                    map.addLayer(layer.textMarker);
+                })
                 map.setView([-7.052358, 110.435002], 21);
                 break;
         }
